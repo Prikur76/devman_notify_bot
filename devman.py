38af2b84dee9defd5bd2d8f09f2c04e10d4d4c66
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-06-20 16:58:43 +0300

    Fix logger, load_dotenv, raise_for_status

--- devman.py
+++ devman.py
@@ -1,15 +1,20 @@
 import argparse
-import logging.config
+import logging
+import os
 import requests
 import textwrap as tw
 import time
+
 from datetime import datetime
+from dotenv import load_dotenv
 from telegram import Bot
 
-import settings
+from settings import TelegramLogsHandler
 
-logging.config.dictConfig(settings.logger_config)
-logger = logging.getLogger('app_logger')
+load_dotenv()
+
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.DEBUG)
 
 
 def get_message_for_chat(review):
@@ -35,22 +40,40 @@ def get_message_for_chat(review):
 
 
 def main():
+    admin_bot = Bot(token=os.environ.get('HELP_BOT_TOKEN'))
+    admin_bot_handler = TelegramLogsHandler(
+        admin_bot, admin_chat_id)
+    admin_bot_handler.setLevel(logging.DEBUG)
+    botformatter = logging.Formatter(
+        fmt='{message}', style='{')
+    admin_bot_handler.setFormatter(botformatter)
+    logger.addHandler(admin_bot_handler)
 
-    long_polling_url = 'https://dvmn.org/api/long_polling/'
-    headers = {'Authorization': f'Token {settings.DVMN_TOKEN}'}
-    payload = {'timestamp': ''}
+    streamhandler = logging.StreamHandler()
+    streamhandler.setLevel(logging.ERROR)
+    streamformatter = logging.Formatter(
+        fmt='{asctime} - {levelname} - {name} - {message}',
+        style='{')
+    streamhandler.setFormatter(streamformatter)
+    logger.addHandler(streamhandler)
 
-    bot = Bot(token=settings.DVMN_BOT_TOKEN)
+    admin_chat_id = os.environ.get('CHAT_ID')
 
     parser = argparse.ArgumentParser(
         description='Получение уведомлений с сайта dvmn.org'
     )
     parser.add_argument('chat_id', nargs='?',
-                        type=int, default=int(settings.CHAT_ID),
+                        type=int, default=admin_chat_id,
                         help='Ввести chat_id')
     args = parser.parse_args()
     user_id = args.chat_id
+
+    long_polling_url = 'https://dvmn.org/api/long_polling/'
+    headers = {'Authorization': f"Token {os.environ.get('DVMN_TOKEN')}"}
+    payload = {'timestamp': ''}
+    main_bot = Bot(token=os.environ.get('DVMN_BOT_TOKEN'))
     logger.debug('Бот DEVMAN запущен')
+
     while True:
         try:
             response = requests.get(
@@ -59,15 +82,20 @@ def main():
                 headers=headers,
                 timeout=25
             )
+            response.raise_for_status()
+
             review = response.json()
             if review['status'] == 'found':
                 payload['timestamp'] = review['last_attempt_timestamp']
                 code_review = review['new_attempts'][0]
                 answer = get_message_for_chat(code_review)
-                bot.send_message(chat_id=user_id, text=answer)
+                main_bot.send_message(chat_id=user_id, text=answer)
             elif review['status'] == 'timeout':
                 payload['timestamp'] = datetime.timestamp(datetime.now())
 
+        except requests.exceptions.HTTPError as http_err:
+            logger.debug('Бот DEVMAN упал с ошибкой:')
+            logger.exception(http_err)
         except requests.exceptions.ConnectionError as conn_err:
             logger.debug('Бот DEVMAN упал с ошибкой:')
             logger.exception(conn_err)
--- settings.py
+++ settings.py
@@ -1,56 +1,15 @@
 import logging
-import os
-
-from telegram import Bot
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
-DVMN_TOKEN = os.environ.get('DEVMAN_TOKEN')
-DVMN_BOT_TOKEN = os.environ.get('DEVMAN_BOT_TOKEN')
-HELP_BOT_TOKEN = os.environ.get('HELP_BOT_TOKEN')
-CHAT_ID = os.getenv('CHAT_ID')
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.tg_bot = tg_bot
+        self.chat_id = chat_id
 
-
-class HelpHandler(logging.Handler):
 
     def emit(self, record):
         message = self.format(record)
-        bot = Bot(token=HELP_BOT_TOKEN)
-        bot.send_message(chat_id=CHAT_ID, text=message)
-
-
-logger_config = {
-    'version': 1,
-    'disable_existing_loggers': False,
-
-    'formatters': {
-        'std_format': {
-            'format': '{asctime} - {levelname} - {name} - {message}',
-            'style': '{'
-        },
-        'simple_format': {
-            'format': '{message}',
-            'style': '{'
-        },
-    },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'level': 'DEBUG',
-            'formatter': 'std_format'
-        },
-        'bot': {
-            '()': HelpHandler,
-            'level': 'DEBUG',
-            'formatter': 'simple_format',
-        }
-    },
-    'loggers': {
-        'app_logger': {
-            'level': 'DEBUG',
-            'handlers': ['console', 'bot']
-        }
-    },
-}
+        self.tg_bot.send_message(
+            chat_id=self.chat_id,
+            text=message)
